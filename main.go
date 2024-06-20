1a1befbb388440dfe06acf9ac40fe491c8698055
dunglv202 <dunglv202@gmail.com>
2024-06-20 14:05:00 +0700

    basic request

--- main.go
+++ main.go
@@ -2,12 +2,30 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 )
 
 func main() {
-	mux := http.NewServeMux();
+	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		delay := r.URL.Query().Get("delay")
+		if delay != "" {
+			delayTime, err := strconv.Atoi(delay)
+			if err == nil && delayTime > 0 {
+				time.Sleep(time.Duration(delayTime) * time.Millisecond)
+			}
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+
+		shouldReturned := r.URL.Query().Get("shouldReturned")
+		if shouldReturned != "" {
+			w.Write([]byte(shouldReturned))
+			return;
+		}
+
 		w.Write([]byte("Hello, World!"))
 	})
 
