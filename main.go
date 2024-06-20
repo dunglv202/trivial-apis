5c3243c4aaba2afc0b86c85e43ca667dfa88dd1a
dunglv <dunglv2@rabiloo.com>
2024-06-20 11:46:01 +0700

    add servemux

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	mux := http.NewServeMux();
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, World!"))
+	})
+
+	http.ListenAndServe(":8000", mux)
 }
